85cf48c5c9f147be6ae03839f0ddaf879bcb2caf
ventuscoder <shivankcode@gmail.com>
2023-03-30 16:09:19 +0530

    Add activate and deactive functionality

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 let buttons = document.querySelectorAll('.drum')
 let keyArr = ['w', 'a', 's', 'd', 'j', 'k', 'l']
 let soundArr = ['tom-1', 'tom-2', 'tom-3', 'tom-4', 'snare', 'crash', 'kick-bass']
+let p = document.querySelector('p')
 
-document.addEventListener('keydown', (e)=>{
-    if (keyArr.includes(e.key.toLowerCase())) {
-        let indexOfKey = keyArr.indexOf(e.key.toLowerCase())
-        playMusic(indexOfKey)
-        animate(keyArr[indexOfKey])
+p.addEventListener('click', (e)=>{
+    if (p.classList[0] == 'unactive') {
+        p.textContent = 'Press me to deactivate drum kit!'
+        document.addEventListener('keydown', (e)=>{
+            if (keyArr.includes(e.key.toLowerCase())) {
+                let indexOfKey = keyArr.indexOf(e.key.toLowerCase())
+                playMusic(indexOfKey)
+                animate(keyArr[indexOfKey])
+            }
+        })
+        
+        for (let i = 0; i < buttons.length; i++) {
+        
+            buttons[i].addEventListener('click', handleBtn)
+            buttons[i].addEventListener('mouseover', handleBtn)
+        }
+    } else {
+        p.textContent = 'Press me to activate drum kit!'
     }
+    p.classList.toggle('unactive')
+    p.classList.toggle('active')
 })
 
-for (let i = 0; i < buttons.length; i++) {
-
-    buttons[i].addEventListener('click', handleBtn)
-    buttons[i].addEventListener('mouseover', handleBtn)
-}
 
 function handleBtn(e) {
     let indexOfKey = keyArr.indexOf(e.srcElement.classList[0])
