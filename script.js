363c41891e4139a22b605e115fea3e5c527eb3c6
ventuscoder <shivankcode@gmail.com>
2023-03-30 15:32:51 +0530

    Set up structure of script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let buttons = document.querySelectorAll('.drum')
+
+for (let i = 0; i < buttons.length; i++) {
+
+    playMusic()
+    animate()
+}
+
+function playMusic() {
+
+}
+
+function animate() {
+
+}
